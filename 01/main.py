b431ecee12cfd2e5a8de322c78442fdcceb4d7da
183644209 <183644209@qq.com>
2015-04-18 23:52:25 +0800

    no message

---
+++ 01/main.py
@@ -0,0 +1,12 @@
+for i in range(1,10):
+        for j in range(i,10):
+            w="{0:1}*{1:1}={2:>2}".format(i,j,i*j)
+            print(w,end="  ")
+        print()
+for i in range(1,10):
+        for k in range(0,i-1):
+            print(end="        ")
+        for j in range(i,10): 
+            print("{0:1}*{1:1}={2:>2}".format(i,j,i*j),end="  ")
+        print()
+
